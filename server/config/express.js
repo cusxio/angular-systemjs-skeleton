1b32efdc3c7778658933b6db9f196f69ef5225dc
Jonathan Chan <cusxio@gmail.com>
2015-09-13 14:30:46 +0800

    remove ghost as an npm module

--- server/config/express.js
+++ server/config/express.js
@@ -3,24 +3,18 @@ var express = require('express'),
     compression = require('compression'),
     bodyParser = require('body-parser'),
     path = require('path'),
-    ghost = require('ghost');
-    // history = require('connect-history-api-fallback');
+    history = require('connect-history-api-fallback');
 
 
 module.exports = function() {
     var app = express();
-
-    ghost().then(function(ghostServer) {
-        app.use('/blog', ghostServer.rootApp);
-        ghostServer.start(app);
-    });
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
     app.use(morgan('dev'));
     app.use(compression());
-    // app.use(history());
+    app.use(history());
     app.use('/', express.static(path.join(__dirname + "/../../dist")));
     return app;
 };

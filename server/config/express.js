9f56b4331248ddde3347629c7085caa305434784
Jonathan Chan <cusxio@gmail.com>
2015-09-13 14:14:41 +0800

    initial commit

--- server/config/express.js
+++ server/config/express.js
@@ -2,18 +2,25 @@ var express = require('express'),
     morgan = require('morgan'),
     compression = require('compression'),
     bodyParser = require('body-parser'),
-    path = require('path');
+    path = require('path'),
+    ghost = require('ghost');
+    // history = require('connect-history-api-fallback');
 
 
 module.exports = function() {
     var app = express();
+
+    ghost().then(function(ghostServer) {
+        app.use('/blog', ghostServer.rootApp);
+        ghostServer.start(app);
+    });
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
     app.use(morgan('dev'));
-
-
-    app.use(express.static(path.join(__dirname + "/../../dist")));
+    app.use(compression());
+    // app.use(history());
+    app.use('/', express.static(path.join(__dirname + "/../../dist")));
     return app;
 };
